6133289fb620ebe12fd43fc62d2f26d3bb7d81a9
sejal0021 <148641950+sejal0021@users.noreply.github.com>
2023-12-05 17:53:03 +0530

    Add files via upload

---
+++ oops.2.py
@@ -0,0 +1,9 @@
+class Car:
+    def set_name(self,name):
+        self.name=name
+    def get_name(self):
+        return self.name
+student1 = Car()
+student1.set_name("sejal")
+print(student1.get_name())
+    
