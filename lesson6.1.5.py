0c21c7aa856e590a874c6998032619c6a281e1a2
sejal0021 <148641950+sejal0021@users.noreply.github.com>
2023-12-05 17:56:38 +0530

    Add files via upload

---
+++ lesson6.1.5.py
@@ -0,0 +1,12 @@
+f1=open("HELLO001.txt",'w')
+l1=["HELLO","\nWELCOME","\nTO","\nPYTHON"]
+f1.writelines(l1)
+f1.close()
+f1=open("HELLO001.txt",'r')
+print(f1.readline())
+print(f1.readline())
+print(f1.readline())
+print(f1.readline())
+f1.close()
+f1=open("HELLO001.txt",'r')
+print(f1.readlines())
